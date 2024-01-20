464a5c8b756fc2c09acf23b3e77d17bf0c8f4e18
Ryan Karpen <ryankarpen2015@gmail.com>
2024-01-19 22:51:14 -0300

    config API

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
+const port = process.env.PORT || 3001
 
 const conn = require("./db/conn")
 conn()
@@ -14,6 +15,6 @@ const routes = require("./routes/router")
 
 app.use("/api", routes)
 
-app.listen(3000, function(){
+app.listen(port, function(){
     console.log("Servidor online")
 })
