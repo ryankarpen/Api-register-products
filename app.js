bc19caf1cba549436e97b67741018941ba8ec08b
Ryan Karpen <ryankarpen2015@gmail.com>
2024-01-20 14:10:26 -0300

    configurando variáveis de ambiente

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const express = require("express")
 const cors = require("cors")
 const app = express()
+require('dotenv').config()
 
 app.use(cors())
 app.use(express.json())
 
-const port = process.env.PORT ?? 3000
+const port = process.env.PORT || 3000
 
 const conn = require("./db/conn")
 conn()
